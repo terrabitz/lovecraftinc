c44192953f9b138032513fc1696bbcec02df732a
Trevor Taubitz <terrabitz@protonmail.com>
2026-01-31 14:58:08 -0500

    fix: Install Tailwind for better defaults and utility classe
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lovecraftinc.example.com',
+
   markdown: {
     shikiConfig: {
       theme: 'dracula',
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });

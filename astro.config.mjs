4b14792fb547e0bd9507eb0a8008b65d7f157eb0
Trevor Taubitz <terrabitz@protonmail.com>
2026-02-01 00:39:36 -0500

    feat: Add cloudflare deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lovecraftinc.example.com',
@@ -15,4 +17,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });

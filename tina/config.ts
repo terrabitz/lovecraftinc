070726f9f5952a50786841cfe94a2a09e8d5ee05
Trevor Taubitz <trevor.taubitz@flocksafety.com>
2026-02-01 12:36:35 -0500

    chore: Add devcontainer config

--- tina/config.ts
+++ tina/config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
   build: {
     outputFolder: "admin",
     publicFolder: "public",
+    host: true, // Listen on all network interfaces
   },
   media: {
     tina: {

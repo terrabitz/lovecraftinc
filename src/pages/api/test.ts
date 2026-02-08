8b8f5e151fc8a929dea1e9fdeae61d973c0156e6
Trevor Taubitz <trevor.taubitz@flocksafety.com>
2026-02-08 14:04:40 -0500

    fix: fix: Add test endpoint with query parameters

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -2,9 +2,15 @@ import type { APIRoute } from 'astro';
 
 export const prerender = false;
 
-export const GET: APIRoute = ({request}) => {
+export const GET: APIRoute = ({params, request}) => {
+  const url = new URL(request.url);
+  const title = url.searchParams.get('title');
   const randomNumber = Math.floor(Math.random() * 100);
-  return new Response(JSON.stringify({ randomNumber }), {
+  return new Response(JSON.stringify({ 
+    randomNumber,
+    title: title,
+    foo: "bar",
+  }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });

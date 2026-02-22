258eecc015719a3087bcc1be5924a4e3dda135ef
Trevor Taubitz <terrabitz@protonmail.com>
2026-02-22 13:42:06 -0500

    fix: Use Keystatic LFS Support (#19)
    * chore: Add additional image patterns to .gitattributes
    
    * chore: Switch to forked keystatic version
    
    * chore: Add permissions to GH token
    
    * chore: Try explicitly passing env vars
    
    * chore: Switch to job-specific permissions
    
    * chore: Proxy keystatic/core imports
    
    * Update src/content/anomalies/EID-112
    
    * chore: Enable LFS support
    
    * Update src/content/anomalies/EID-978

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,4 +1,4 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from '@terrabitz/keystatic-core';
 
 function contentField(collectionName: string) {
   return fields.markdoc({
@@ -25,7 +25,8 @@ const storage = import.meta.env.PUBLIC_KEYSTATIC_MODE === 'github' ? {
     repo: {
       owner: import.meta.env.PUBLIC_GITHUB_OWNER || '',
       name: import.meta.env.PUBLIC_GITHUB_REPO || '',
-    }
+    },
+    lfs: true,
   } : {
     kind: 'local' as const
   }

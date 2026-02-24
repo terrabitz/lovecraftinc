49e2954b7a96f2121dfa9f1b775627a25c8b234a
Trevor Taubitz <trevor.taubitz@flocksafety.com>
2026-02-23 20:32:56 -0500

    feat: Add basic Markdoc component

---
+++ markdoc.config.mjs
@@ -0,0 +1,13 @@
+import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
+
+export default defineMarkdocConfig({
+  tags: {
+    callout: {
+      render: component('./src/components/Callout.astro'),
+      attributes: {
+        type: { type: String, default: 'info' },
+        title: { type: String, required: true },
+      },
+    },
+  },
+});
